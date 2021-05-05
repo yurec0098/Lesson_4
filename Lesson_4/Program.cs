4bd94cc560fe7bfcb80768e0b8cc358ee77947bf
yurec0098 <yurec0098@rambler.ru>
2021-05-05 16:57:15 +0300

    Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -6,7 +6,15 @@ namespace Lesson_4
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine(GetFullName("Иванов", "Иван", "Иванович"));
+			Console.WriteLine(GetFullName("Сидоров", "Пётр", "Николаевич"));
+			Console.WriteLine(GetFullName("Крюкова", "Ольга", "Петровна"));
+			Console.WriteLine(GetFullName("Дудник", "Андрей", "Романович"));
+		}
+
+		static string GetFullName(string firstName, string lastName, string patronymic)
+		{
+			return $"{firstName} {lastName} {patronymic}";
 		}
 	}
 }
