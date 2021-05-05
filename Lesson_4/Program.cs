259ce03d61f7a2fc2d086573a3979dd54672639b
yurec0098 <yurec0098@rambler.ru>
2021-05-05 18:56:14 +0300

    Написать метод по определению времени года. На вход подаётся число – порядковый номер месяца. На выходе — значение из перечисления (enum) — Winter, Spring, Summer, Autumn. Написать метод, принимающий на вход значение из этого перечисления и возвращающий название времени года (зима, весна, лето, осень). Используя эти методы, ввести с клавиатуры номер месяца и вывести название времени года. Если введено некорректное число, вывести в консоль текст «Ошибка: введите число от 1 до 12».

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -2,11 +2,77 @@
 
 namespace Lesson_4
 {
+	enum Season
+	{
+		None,
+		Winter,
+		Spring,
+		Summer,
+		Autumn
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Введите номер месяца:");
+
+			var month = Console.ReadLine();
+			while(GetSeason(month) == Season.None)
+				month = Console.ReadLine();
+
+			Console.WriteLine($"Порядковый номер месяца ({month}) соответствует времени года {GetStringSeason(GetSeason(month))}");
+			Console.ReadLine();
+		}
+
+		static Season GetSeason(string month)
+		{
+			switch (month)
+			{
+				case "1":
+				case "2":
+				case "12":
+					return Season.Winter;
+
+				case "3":
+				case "4":
+				case "5":
+					return Season.Spring;
+
+				case "6":
+				case "7":
+				case "8":
+					return Season.Summer;
+
+				case "9":
+				case "10":
+				case "11":
+					return Season.Autumn;
+
+				default:
+					Console.WriteLine("Ошибка: введите число от 1 до 12");
+					return Season.None;
+			}
+		}
+		static string GetStringSeason(Season season)
+		{
+			switch (season)
+			{
+				case Season.Winter:
+					return "Зима";
+
+				case Season.Spring:
+					return "Весна";
+
+				case Season.Summer:
+					return "Лето";
+
+				case Season.Autumn:
+					return "Осень";
+
+				default:
+					return "Ошибка: введите число от 1 до 12";
+			}
 		}
 	}
 }
