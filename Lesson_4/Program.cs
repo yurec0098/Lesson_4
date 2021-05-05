5f4ab466e04414640ab4bbe5d359b145a7beaa80
yurec0098 <yurec0098@rambler.ru>
2021-05-05 17:04:16 +0300

    Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат на экран

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -6,7 +6,22 @@ namespace Lesson_4
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Введите массив чисел, разделённых пробелом:");
+			int sum = StringNumberSum(Console.ReadLine());
+			Console.WriteLine($"Сумма введённых Вами чисел равна: {sum}");
+			Console.ReadLine();
+		}
+
+		static int StringNumberSum(string numbers)
+		{
+			int result = 0;
+			int tmp_val;
+
+			foreach (var x in numbers.Split(' '))
+				if (int.TryParse(x, out tmp_val))
+					result += tmp_val;
+
+			return result;
 		}
 	}
 }
